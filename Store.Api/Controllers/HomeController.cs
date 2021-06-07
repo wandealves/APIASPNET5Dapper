a5115f93bf983c6588066d0214c472d130459bab
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-07 11:50:41 -0300

    add routes

---
+++ Store.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Api.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("")]
+    public string Get()
+    {
+      return "Hello World";
+    }
+  }
+}
--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -11,7 +11,7 @@ namespace Store.Api
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+      services.AddControllers();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,7 +22,11 @@ namespace Store.Api
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseMvc();
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+       {
+         endpoints.MapControllers();
+       });
     }
   }
 }
